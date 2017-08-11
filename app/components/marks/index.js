96ef61661114c654351c4b5e9be94e8c065c4411
Kirill Gusev <kirikg221@mail.ru>
2017-08-11 22:38:09 +0300

    first commit

--- app/components/marks/index.js
+++ app/components/marks/index.js
@@ -1,12 +1,65 @@
 import './style.css';
-
+import mediator from '../mediator';
 
 class Marks {
-    constructor() {
-        // @fixme remove `console.log`
-        // eslint-disable-next-line
-        console.log('"Marks" created');
+  constructor(width, height, id) {
+    this.id = id;
+    this.domMark =  d3.select("div");
+    const svg = this.domMark.append("svg")
+    .style("width", width)
+    .style("height", height)
+    .on("mousedown", mousedown);
+
+
+    const line = d3.line()
+    .x(d => d[0])
+    .y(d => d[1])
+    .curve(d3.curveLinear);
+
+    function mousedown() {
+      if (flag === 0) {
+        return -1;
+      }
+
+      let data = [];
+      const path = d3.select(this)
+      .append("path")
+      .attr("stroke", "green")
+      .attr("stroke-width", 4)
+      .attr("fill", "none");
+
+      d3.select(this)
+      .on("mousemove", () => {
+        data.push(d3.mouse(this));
+        let coordinate = d3.mouse(this);
+
+        if (coordinate[0] > width - 5 || coordinate[0] <= 5 || coordinate[1] > height - 5 || coordinate[1] <= 5) {
+          path.attr("d", line(data));
+          d3.select(this).on("mousemove", null).on("mouseup", null);
+          data = [];
+          return;
+        }
+
+        path.attr("d", line(data));
+      })
+      .on("mouseup", () => {
+        mediator.emit('newLine', {
+          id: this.id,
+          line: data
+        });
+        d3.select(this).on("mousemove", null);
+        data = [];
+      });
     }
+    mediator.on('newLine', this.drowNewLine.bind(this));
+  }
+
+  drowNewLine(obj) {
+    if (this.id === obj.id) {
+      path.attr("d", obj.line);
+    }
+  }
+
 }
 
 export default Marks;
